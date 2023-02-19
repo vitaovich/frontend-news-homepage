23f06c04936e1811ac4191b6f556e5d38147472b
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-19 18:50:36 +0000

    initial setup for fonts, colors and images.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { Inter } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        softOrange: 'hsl(35, 77%, 62%)',
+        softRed: 'hsl(5, 85%, 63%)',
+        // Neutral
+        offWhite: 'hsl(36, 100%, 99%)',
+        grayishBlue: 'hsl(233, 8%, 79%)',
+        darkGrayishBlue: 'hsl(236, 13%, 42%)',
+        veryDarkBlue: 'hsl(240, 100%, 5%)',
+      },
+    },
+    fontFamily: {
+      inter: ['var(--font-inter)'],
+    },
   },
   plugins: [],
 }
