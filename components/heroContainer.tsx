560aba5419d57f52c97c35723d5219de5719eccd
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-20 04:27:51 +0000

    refactor home

---
+++ components/heroContainer.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import ArticleContainer from '@/components/articleContainer'
+
+const HeroContainer = () => {
+    return (
+        <div className="flex flex-col md:flex-row md:space-x-4">
+            <div className='md:w-2/3'>
+                <Image
+                    alt='top laptops'
+                    src='/images/image-web-3-mobile.jpg'
+                    width={686}
+                    height={500}
+                    className='md:hidden'
+                />
+                <Image
+                    alt='top laptops'
+                    src='/images/image-web-3-desktop.jpg'
+                    width={1460}
+                    height={600}
+                    className='hidden md:block'
+                />
+                <div className="flex flex-col md:flex-row md:mt-8">
+                    <div className='md:w-1/2'>
+                        <h1 className="text-5xl font-bold">The Bright Future of Web 3.0?</h1>
+                    </div>
+                    <div className='md:w-1/2 md:space-y-8'>
+                        <p className="text-darkGrayishBlue">
+                            We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+                            But is it really fulfilling its promise?
+                        </p>
+                        <div className="bg-softRed text-offWhite py-2 px-6 w-1/2 uppercase text-center tracking-widest">Read more</div>
+                    </div>
+                </div>
+            </div>
+            <ArticleContainer />
+        </div>
+    )
+}
+
+export default HeroContainer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,74 +1,49 @@
 import ArticleContainer from '@/components/articleContainer'
+import HeroContainer from '@/components/heroContainer'
 import Nav from '@/components/nav'
 import Topic from '@/components/topic'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [topics, setTopics] = useState(
+    [
+      {
+        imagePath: '/images/image-retro-pcs.jpg', 
+        imageAlt: 'retro pcs', 
+        header: '01', 
+        title: 'Reviving Retro PCs', 
+        content: 'What happens when old PCs are given modern upgrades?'
+      },
+      {
+        imagePath: '/images/image-top-laptops.jpg', 
+        imageAlt: 'top laptops', 
+        header: '02', 
+        title: 'Top 10 Laptops of 2022', 
+        content: 'Our best picks for various needs and budgets.'
+      },
+      {
+        imagePath: '/images/image-gaming-growth.jpg', 
+        imageAlt: 'gaming growth', 
+        header: '03', 
+        title: 'The Growth of Gaming', 
+        content: 'How the pandemic has sparked fresh opportunities.'
+      },
+    ]
+  )
+
   return (
     <div className='flex flex-col items-center'>
       <div className="flex flex-col justify-center m-4 space-y-8 font-inter xl:w-3/4">
-        {/* NavBar Section */}
-        <Nav></Nav>
-        <div className="flex flex-col md:flex-row md:space-x-4">
-          {/* Main Area */}
-          <div className='md:w-2/3'>
-            <Image
-              alt='top laptops'
-              src='/images/image-web-3-mobile.jpg'
-              width={686}
-              height={500}
-              className='md:hidden'
-            />
-            <Image
-              alt='top laptops'
-              src='/images/image-web-3-desktop.jpg'
-              width={1460}
-              height={600}
-              className='hidden md:block'
-            />
-            <div className="flex flex-col md:flex-row md:mt-8">
-              <div className='md:w-1/2'>
-                <h1 className="text-5xl font-bold">The Bright Future of Web 3.0?</h1>
-              </div>
-              <div className='md:w-1/2 md:space-y-8'>
-                <p className="text-darkGrayishBlue">
-                  We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
-                  But is it really fulfilling its promise?
-                </p>
-                <div className="bg-softRed text-offWhite py-2 px-6 w-1/2 uppercase text-center tracking-widest">Read more</div>
-              </div>
-            </div>
-          </div>
-          {/* Articles section */}
-          <ArticleContainer></ArticleContainer>
-        </div>
-
-
-        {/* Topics section */}
+        <Nav />
+        <HeroContainer />
         <div className="flex flex-col space-y-4 md:flex-row md:space-x-2">
-          <Topic 
-          imagePath={'/images/image-retro-pcs.jpg'} 
-          imageAlt={'retro pcs'} 
-          header={'01'} 
-          title={'Reviving Retro PCs'} 
-          content={'What happens when old PCs are given modern upgrades?'}
-          />
-          <Topic 
-          imagePath={'/images/image-top-laptops.jpg'} 
-          imageAlt={'top laptops'} 
-          header={'02'} 
-          title={'Top 10 Laptops of 2022'} 
-          content={'Our best picks for various needs and budgets.'}
-          />
-          <Topic 
-          imagePath={'/images/image-gaming-growth.jpg'} 
-          imageAlt={'gaming growth'} 
-          header={'03'} 
-          title={'The Growth of Gaming'} 
-          content={'How the pandemic has sparked fresh opportunities.'}
-          />
+          {
+            topics.map((topic) => (
+              <Topic key={topic.header} imagePath={topic.imagePath} imageAlt={topic.imageAlt} header={topic.header} title={topic.title} content={topic.content} />
+            ))
+          }
         </div>
-
       </div>
     </div>
   )
